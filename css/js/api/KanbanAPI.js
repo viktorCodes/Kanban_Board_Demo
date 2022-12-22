13e6b91c67feaf97158021243c6cc4d9a306708f
calimendoza <mbaivictor1@gmail.com>
2022-12-21 20:46:41 -0800

    finished Kanban API

--- css/js/api/KanbanAPI.js
+++ css/js/api/KanbanAPI.js
@@ -27,6 +27,44 @@ export default class KanbanAPI{
     return item
   }
 
+  static updateItem(itemId, newProps){
+    const data= read()
+    const [item, currentColumn] = (() => {
+        for(const column of data){
+            const item = column.items.find(item => item.id == itemId)
+
+            if(item){
+                return [item, column]
+            }
+        }
+    })()
+
+    if(!item){
+        throw new Error('Item not found')
+    }
+
+    item.content = newProps.content === undefined ? item.content : newProps.content
+
+    if(
+        newProps.columnId !== undefined
+        && newProps.position !== undefined
+    ) {
+        const targetColumn = data.find(column => column.id == newProps.columnId)
+
+        if(!targetColumn) {
+            throw new Error("Target column not found")
+        }
+
+        currentColumn.items.splice(currentColumn.items.indexOf(item),1)
+
+        targetColumn.items.splice(newProps.position,0,item)
+    }
+
+    save(data)
+
+    
+  }
+
 
   static deleteItem(itemId){
     const data = read()
@@ -35,11 +73,14 @@ export default class KanbanAPI{
         const item = column.items.find(item => item.id == itemId)
 
         if(item){
-            column.items.splice(columns.item.indexOf(item), 1)
+            column.items.splice(column.item.indexOf(item), 1)
         }
     }
     save(data)
+
+    
   }
+
 }
 
 function read(){
